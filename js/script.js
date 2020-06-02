9fb2dc922faa7d4767e7069ec1a23e934c545362
vgaimanovskyi <vgaymanovskiy@gmail.com>
2020-06-02 11:39:36 +0300

    add 42-2-homework

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        list: [{
+            name: "22 lesson ",
+            link: "https://vgaimanovskyi.github.io/22-1-Homework/index.html",
+            prompt: "Основи JavaScript",
+        }, {
+            name: "23 lesson ",
+            link: "https://vgaimanovskyi.github.io/23-1-Homework/index.html",
+            prompt: "Типи даних та оператори",
+        }, {
+            name: "24 lesson ",
+            link: "https://vgaimanovskyi.github.io/24-1-Homework/index.html",
+            prompt: "Цикли",
+        }, {
+            name: "25 lesson ",
+            link: "https://vgaimanovskyi.github.io/25-1-Homework/index.html",
+            prompt: "Функції",
+        }, {
+            name: "26 lesson ",
+            link: "https://vgaimanovskyi.github.io/26-1-Homework/index.html",
+            prompt: "Об’єкти",
+        }, {
+            name: "27 lesson ",
+            link: "https://vgaimanovskyi.github.io/27-1-Homework/index.html",
+            prompt: "Масиви",
+        }, {
+            name: "28 lesson ",
+            link: "https://vgaimanovskyi.github.io/28-1-Homework/index.html",
+            prompt: "Класи",
+        }, {
+            name: "29 lesson ",
+            link: "https://vgaimanovskyi.github.io/29-1-Homework/index.html",
+            prompt: "Document Object Model",
+        }, {
+            name: "30 lesson ",
+            link: "https://vgaimanovskyi.github.io/30-1-Homework/index.html",
+            prompt: "Події",
+        }]
+    }
+})
